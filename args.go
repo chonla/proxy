4960183d02ca976fc898a0d0158f1570618ddec0
sittisin <sittisin@dtac.co.th>
2016-02-26 14:51:08 +0700

    remove code comment

--- args.go
+++ args.go
@@ -24,30 +24,3 @@ func parseArg() {
 	println("===========================================")
 	fmt.Printf("arg = %#v\n", arg)
 }
-
-/*
-import (
-	"flag"
-	"fmt"
-)
-
-type Arg struct {
-	Endpoint     string
-	Mode         string
-	ProxyPort    string
-	StubFileName string
-}
-
-func parseArg() {
-	flag.StringVar(&arg.Endpoint, "target", "https://www.google.com/starwars", "target URL for reverse proxy")
-	flag.StringVar(&arg.ProxyPort, "port", "9000", "proxy running on port. EX: 9000")
-	flag.StringVar(&arg.Mode, "mode", "Record", "proxy running mode [Record/Replay] default is Replay")
-	flag.StringVar(&arg.StubFileName, "stubFileName", "stub.json", "record to file name EX stub.json")
-	flag.Parse()
-
-	println("===========================================")
-	println(arg.Mode, "Mode")
-	println("===========================================")
-	fmt.Printf("arg = %#v\n", arg)
-}
-*/
